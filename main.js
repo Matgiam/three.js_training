0d3f07509adea1f3ea61727b25ff0e905b9f820e
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-09-30 10:48:59 +0200

    comments: added comments under the functions to understand how they work

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// 75: FOV (field of view) window.innerWidth / window.innerHeight: aspect ratio 0.1: near plane 1000: far plane
+//(0.1,1000) all the objects within this near and far plane will be rendered, otherwise not.
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x10ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//renderer.domElement is the HTML <canvas> element that Three.js creates for rendering the 3D scene.
+//document.body.appendChild(...) takes that canvas and inserts it into the page’s <body>, so it actually shows up in the browser.
 
-camera.position.z = 5;
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x10ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 2;
 
 function animate() {
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-
+	cube.rotation.y += 0.01;
+	renderer.render(scene, camera);
 }
